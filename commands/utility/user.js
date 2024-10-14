b47159d3d5f5dfbcdfb26e78d0050af6f2cf04d4
João Luís A. <116316121+lyszt@users.noreply.github.com>
2024-10-13 21:38:32 -0300

    Testing commands added

---
+++ commands/utility/user.js
@@ -0,0 +1,10 @@
+const {SlashCommandBuilder} = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('user')
+        .setDescription('Provides information about the user.')
+    async execute(interation){
+        await interaction.reply(`This command was run by ${interaction.user.username}, who joined on ${interaction.member.joinedAt}.`);
+    },
+};
